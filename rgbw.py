0cc9bb5e56915afe78baa6c84c6fd2d2690dee1f
Nazmus Nasir <nazmus.nasir@gmail.com>
2019-10-19 17:23:19 -0400

    separated white button for simplicity

--- rgbw.py
+++ rgbw.py
@@ -12,14 +12,24 @@ def led():
     red = int(request.args.get('red')) if (request.args.get('red')) else 0
     green = int(request.args.get('green')) if (request.args.get('green')) else 0
     blue = int(request.args.get('blue')) if (request.args.get('blue')) else 0
-    white = int(request.args.get('white')) if (request.args.get('white')) else 0
+    # white = int(request.args.get('white')) if (request.args.get('white')) else 0
 
     pi = pigpio.pi()
 
     pi.set_PWM_dutycycle(24, red)
     pi.set_PWM_dutycycle(20, blue)
     pi.set_PWM_dutycycle(25, green)
+    # pi.set_PWM_dutycycle(18, white)
+
+    # return jsonify({"red": red, "green": green, "blue": blue, "white": white})
+    return jsonify({"red": red, "green": green, "blue": blue})
+
+
+# Separated white button for now so it can be controlled separately
+@app.route('/white', methods=['GET'])
+def white():
+    white = int(request.args.get('white')) if (request.args.get('white')) else 0
+    pi = pigpio.pi()
+
     pi.set_PWM_dutycycle(18, white)
-
-    return jsonify({"red": red, "green": green, "blue": blue, "white": white})
-
+    return jsonify({"white": white})
--- script.js
+++ script.js
@@ -2,9 +2,10 @@ let config = {
     url: 'http://192.168.1.225',
 };
 
-let globalStatus = 0;
+let whiteStatus = 0;
 
 $(document).ready(function() {
+    btnStatus();
     // Cache buster added because caching was a big problem on mobile
     let cacheBuster = new Date().getTime();
     const pickr = Pickr.create({
@@ -55,10 +56,11 @@ $(document).ready(function() {
 
     function sendData(e){
         let obj = e.toRGBA();
-        let red = obj[0];
-        let green = obj[1];
-        let blue = obj[2];
+        let red = Math.floor(obj[0]);
+        let green = Math.floor(obj[1]);
+        let blue = Math.floor(obj[2]);
         let queryBuilder = `red=${red}&green=${green}&blue=${blue}`;
+        console.log(queryBuilder);
 
         $.ajax({
             url: `${config.url}/api/lr?${queryBuilder}&${cacheBuster}`,
@@ -69,6 +71,38 @@ $(document).ready(function() {
                 console.log(result);
             }
         });
+    }
+
+     $('#btnToggle').on('click', function(e){
+
+        // let state;
+        if(whiteStatus == 0) {
+            whiteStatus = 255;
+        } else {
+            whiteStatus = 0;
+        }
+
+        //right
+        $.ajax({
+            url: `${config.url}/api/lr/white?white=${whiteStatus}&${cacheBuster}`,
+            method: 'GET',
+            success: function(result) {
+                console.log(result);
+            },
+            complete: btnStatus
+        });
+        e.preventDefault();
+    });
+
+    // Main big button - uses kitchenRight for master data.
+    function btnStatus() {
+        if(whiteStatus == 0) {
+            $('#btnToggle').text('Turn On');
+            $('#btnToggle').removeClass().addClass('btn btn-block btn-dark');
+        } else {
+            $('#btnToggle').text('Turn Off')
+            $('#btnToggle').removeClass().addClass('btn btn-block btn-light');
+        }
     }
 
 });
